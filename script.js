c7dc05e9d77751bd7a46130657539ecf87a4ab67
Lateef Ishola <isholalateef9@gmail.com>
2025-04-26 14:38:20 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
+
+function displayContact() {
+  document.getElementById("contact-display").style.display = "flex";
+}
